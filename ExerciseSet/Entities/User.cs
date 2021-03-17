7e6444961b405c8fd511c97fe53c8822ec9bbcb0
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-03-17 17:51:50 -0300

    Using HashSet for log Users

---
+++ ExerciseSet/Entities/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseSet.Entities
+{
+    class User
+    {
+        public string Name { get; set; }
+        public DateTime Instant { get; set; }
+
+        public User(string name, DateTime instant)
+        {
+            Name = name;
+            Instant = instant;
+        }
+
+        
+        public override bool Equals(object obj)
+        {
+            if (!(obj is User))
+            {
+                throw new ArgumentException("Item in not User valid");
+            }
+
+            User other = obj as User;
+
+            return Name.Equals(other.Name);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+    }
+}
---
+++ ExerciseSet/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using ExerciseSet.Entities;
+
+namespace ExerciseSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the path of file: ");
+            string path = Console.ReadLine();
+            HashSet<User> hashUser = new HashSet<User>();
+            User user;
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        
+                        user = new User(name, instant );
+                        hashUser.Add(user);
+
+                        Console.WriteLine(user.Name);
+
+                    }
+
+                    Console.WriteLine( $"\nTotal users: {hashUser.Count}"); 
+                }
+            }
+
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
